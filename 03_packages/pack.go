7d9a49edfc6bacf7a68e6c3dd63626f35b87f8fc
atul161 <atulanand199898dbg@gmail.com>
2019-06-05 09:04:34 +0530

    packages uploaded

---
+++ 03_packages/pack.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Packages Completed")
+}
+
+/*
+There are many ways to mport packages in a go
+import "fmt";import "os"
+or
+import(
+	"fmt"
+	"os"
+)
+
+concept:packages are ussed  to categorize our program
+and easy to main tain
+*/
