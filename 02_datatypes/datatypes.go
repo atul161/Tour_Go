85e840780c37625f76f042c98ecffaa17c090432
atul161 <atulanand199898dbg@gmail.com>
2019-06-05 08:55:43 +0530

    Datatypes added

---
+++ 02_datatypes/datatypes.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("In Coding Everything is a World\n lets Start with the world of datatype")
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%T %T %T %T\n", i, f, b, s)
+	fmt.Printf("\n %v %v %v %q", i, f, b, s)
+}
+
+/*
+
+syntax of varaible declaration is
+var name datatype
+ie var a int
+   var b bool
+   ther e are 25 keyword in go
+
+*/
